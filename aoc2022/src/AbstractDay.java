240326f1e766b21788166274b1e0df4bf831973e
Jason Woitalla <jason.woitalla@gmail.com>
2022-12-06 11:24:55 -0600

    Day 6

--- aoc2022/src/AbstractDay.java
+++ aoc2022/src/AbstractDay.java
@@ -7,11 +7,16 @@ public abstract class AbstractDay {
     public AbstractDay(String path, int part) {
         try {
             Scanner scan = new Scanner(new File(path));
+            final long startTime = System.currentTimeMillis();
             if(part == 1) {
+                System.out.println("[AOC] Running Part 1");
                 part1(scan);
             } else if(part == 2) {
+                System.out.println("[AOC] Running Part 2");
                 part2(scan);
             }
+            final long endTime = System.currentTimeMillis();
+            System.out.println("[AOC] Finished, ran in: " + (endTime - startTime) + "ms");
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
--- aoc2022/src/App.java
+++ aoc2022/src/App.java
@@ -3,6 +3,7 @@ public class App {
         //Day2 day2 = new Day2("inputs/day2.txt");
         //Day3 day3 = new Day3("inputs/day3.txt");
         //Day4 day4 = new Day4("inputs/day4.txt");
-        Day5 day5 = new Day5("inputs/day5.txt");
+        //new Day5("inputs/day5.txt");
+        new Day6();
     }
 }
